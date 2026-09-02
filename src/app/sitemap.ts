8003b431e13314ef9e91d05770c7376fea524d07
DiegoSalsa <diego.guzman2201@alumnos.ubiobio.cl>
2026-09-01 20:28:25 -0400

    chore(seo): refresh sitemap modification date

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import { guides } from "@/lib/data/guides";
 import { absoluteUrl } from "@/lib/site";
 import { featureSolutions } from "@/lib/data/feature-solutions";
 
-const contentUpdatedAt = new Date("2026-08-31T00:00:00-04:00");
+const contentUpdatedAt = new Date("2026-09-01T00:00:00-04:00");
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const staticRoutes: MetadataRoute.Sitemap = [
